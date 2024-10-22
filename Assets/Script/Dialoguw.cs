a1e742d098a9a8bfeaaa922dae124f34d4ff49b2
ALEXBR36 <alexander.briscoe@hibs.school.nz>
2024-10-22 20:57:43 +1300

    last ones

--- Assets/Script/Dialoguw.cs
+++ Assets/Script/Dialoguw.cs
@@ -5,44 +5,44 @@ using TMPro;
 
 public class Dialoguw : MonoBehaviour
 {
-    public TextMeshProUGUI text;
-    public string[] lines;
-    public float textspeed;
+    public TextMeshProUGUI text; //naimg the variables
+    public string[] lines; //array which holds dialogue
+    public float textspeed; //contols speed of text
 
-    private int index;
+    private int index; //tracks the line being displayed
 
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        text.text = string.Empty;
-        StartDialogue();
+        text.text = string.Empty; //starts text by being empty
+        StartDialogue(); //calls this function
     }
 
     // Update is called once per frame
-    public void Work()
+    public void Work() //used to register button click
     {
         {
-            if (text.text == lines[index])
+            if (text.text == lines[index]) //checks if line is fully finsihed
             {
-                NextLine();
+                NextLine(); //if so calls the next line
             }
             else
             {
-                StopAllCoroutines();
-                text.text = lines[index];
+                StopAllCoroutines(); //if not it stops all
+                text.text = lines[index]; //displays current line immedietly
             }
         }
     }
 
     void StartDialogue()
     {
-        index = 0;
-        StartCoroutine(TypeLine());
+        index = 0; //sets to first line
+        StartCoroutine(TypeLine()); //starts coroutine
     }
 
     IEnumerator TypeLine()
     {
-        foreach (char c in lines[index].ToCharArray())
+        foreach (char c in lines[index].ToCharArray()) //all this adds a charcter to the text and waits in between whatever the text speed is
         {
             text.text += c;
             yield return new WaitForSeconds(textspeed);
@@ -50,15 +50,15 @@ public class Dialoguw : MonoBehaviour
     }
     void NextLine()
     {
-        if (index < lines.Length - 1)
+        if (index < lines.Length - 1) //advances to next if more lines are left to go
         {
-            index++;
-            text.text = string.Empty;
-            StartCoroutine(TypeLine());
+            index++; //next
+            text.text = string.Empty; //clears the previous line of text
+            StartCoroutine(TypeLine()); //starts next line
         }
         else
         {
-            gameObject.SetActive(false);
+            gameObject.SetActive(false); //ends dialogue
         }
     }
 }
